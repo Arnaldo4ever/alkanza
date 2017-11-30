56610df4cc561956fcb03ec63ba0a4ec8c178926
Arnaldo <arnaldocarrizales@gmail.com>
2017-11-30 18:17:40 -0500

    Filtro terminado

--- wp-content/themes/selfie/team.php
+++ wp-content/themes/selfie/team.php
@@ -39,11 +39,32 @@
 			<div class="homepage-container-design-inner" style="max-width: 1140px;">
 <div class="vc_row wpb_row vc_inner vc_row-fluid">
   <div class="wpb_column vc_column_container vc_col-sm-12">
-    <div align="center">
-      <?php if (have_posts() ) while ( have_posts() ) : the_post(); ?>
-            <button class="btn btn-default filter-button" data-filter="<?php echo $cache_categories[$cat]->cat_name ?>"><?php echo $cache_categories[$cat]->cat_name ?></button>
-            <?php endwhile; ?>
-        </div>
+    <div align="left">
+      <?php
+
+    // Ahora buscamos las categorías inferiores
+    $argumentos = array(
+      'child_of' => $categoria_id
+      );
+
+    // Creamos la matriz $categorias_hijas
+    $categorias_hijas = array();
+    $categorias_hijas = get_categories( $argumentos );
+
+    // Y las mostramos
+    if ( !empty( $categorias_hijas ) ) {
+      ?>
+      <?php
+      foreach( $categorias_hijas as $subcategoria ) {
+        ?>
+          <button class="btn btn-default filter-button" data-filter="<?php echo $subcategoria->cat_name; ?>"><?php echo $subcategoria->cat_name; ?></button>
+      <?php
+       }
+      ?>
+      <?php
+    }
+  ?>
+      </div>
     <div class="wpb_wrapper">
       <div class="">
         <?php $args = array( 'post_type' => 'team', 'posts_per_page' => 99, 'order'=> 'DESC', 'orderby' => 'id' ); ?>
@@ -52,37 +73,17 @@
         <div class="col-md-3 col-sm-6 col-xs-12 filter <?php $category = get_the_category(); echo $category[0]->cat_name; ?>">
 							<div class="member_item">
 							<div class="member_img">
-								<img width="500" height="500" src="http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-2.jpg" class="attachment-identity-team size-identity-team wp-post-image" alt="Team-2" srcset="http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-2-150x150.jpg 150w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-2-300x300.jpg 300w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-2-75x75.jpg 75w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-2-220x220.jpg 220w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-2-380x380.jpg 380w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-2-180x180.jpg 180w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-2-400x400.jpg 400w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-2.jpg 500w" sizes="(max-width: 500px) 100vw, 500px">
+								<img src="<?php the_post_thumbnail(); ?>" class="attachment-identity-team size-identity-team wp-post-image">
 							</div>
 							<div class="member_descr center">
 								<div class="member_name">
-									Rose Doe
+									<?php the_title(); ?>
 								</div>
 								<div class="member_post">
 									  Graphic Designer
 								</div>
-								<div class="member_social">
-								<a href="http://themes.profteamsolutions.com/selfie-multi/" class="facebook"><i class="fa fa-facebook"></i></a><a href="http://themes.profteamsolutions.com/selfie-multi/" class="twitter"><i class="fa fa-twitter"></i></a><a href="http://themes.profteamsolutions.com/selfie-multi/" class="linkedin"><i class="fa fa-linkedin"></i></a>
-								</div>
 								<div class="member_about">
-									 In et massa a massa egestas suscipit tincidunt ut est. Curabitur rutrum faucibus elit, at convallis diam.
-								</div>
-								<div class="skill-member">
-									<ul class="skillBar">
-										<li style="opacity: 1; left: 0px;">
-									<div class="skillBg">
-										<span data-width="90" style="width: 90%;"><strong>Photoshop 90%</strong></span>
-									</div>
-								</li><li style="opacity: 1; left: 0px;">
-									<div class="skillBg">
-										<span data-width="85" style="width: 85%;"><strong>HTML/CSS 85%</strong></span>
-									</div>
-								</li><li style="opacity: 1; left: 0px;">
-									<div class="skillBg">
-										<span data-width="92" style="width: 92%;"><strong>Management 92%</strong></span>
-									</div>
-								</li>
-									</ul>
+									 <?php the_content(); ?>
 								</div>
 							</div>
 							</div>
