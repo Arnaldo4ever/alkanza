13614611e4b9ea5a1c7fa6eeff9ead34fc0279ff
Arnaldo <arnaldocarrizales@gmail.com>
2017-11-30 15:39:34 -0500

    Agregada pagina de Team

---
+++ wp-content/themes/selfie/team.php
@@ -0,0 +1,182 @@
+<?php
+/**
+ * Template Name: Template - Team
+ */
+?>
+
+
+<!-- Get Page Header
+================================================== -->
+<?php get_header('nuevo'); ?>
+
+<div class="vc_row wpb_row vc_row-fluid make-margin-bottom-zero vc_custom_1451850512524" style="margin-top: 170px; display: flex; justify-content: center;">
+  <div class="wpb_column vc_column_container vc_col-sm-12" style="background-color: #d6d6d6; border-radius: 10px; width: 90%; padding: 50px;">
+    <div class="homepage-container-design-inner" style="width: 100%;">
+      <div class="section-title  text-right">
+      				<span class="fa fa-coffee fa-2x" style="color: #999999;"></span>
+      				<h1 style="color: #999999;">OUR TEAM</h1>
+      </div>
+	<div class="wpb_text_column wpb_content_element  vc_custom_1451648241754">
+		<div class="wpb_wrapper">
+			<p class="selfie-subtitle-text999" style="text-align: right;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Duis ut lobortis nulla luctus et ultrices posuere cubilia,<br> Vestibulum ante ipsum primis in faucibus orci
+        luctus et ultrices posuere cubilia Curae
+      </p>
+		</div>
+	</div>
+    </div>
+    <div class="wpb_wrapper"><div class="homepage-container-design  " style="color:#666666;">
+			<div class="homepage-container-design-inner">
+<div class="vc_row wpb_row vc_inner vc_row-fluid"><div class="wpb_column vc_column_container vc_col-sm-12"><div class="wpb_wrapper"><div class=""><div class="col-md-3 col-sm-6 col-xs-12">
+							<div class="member_item">
+							<div class="member_img">
+								<img width="500" height="500" src="http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-2.jpg" class="attachment-identity-team size-identity-team wp-post-image" alt="Team-2" srcset="http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-2-150x150.jpg 150w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-2-300x300.jpg 300w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-2-75x75.jpg 75w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-2-220x220.jpg 220w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-2-380x380.jpg 380w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-2-180x180.jpg 180w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-2-400x400.jpg 400w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-2.jpg 500w" sizes="(max-width: 500px) 100vw, 500px">
+							</div>
+							<div class="member_descr center">
+								<div class="member_name">
+									Rose Doe
+								</div>
+								<div class="member_post">
+									  Graphic Designer
+								</div>
+								<div class="member_social">
+								<a href="http://themes.profteamsolutions.com/selfie-multi/" class="facebook"><i class="fa fa-facebook"></i></a><a href="http://themes.profteamsolutions.com/selfie-multi/" class="twitter"><i class="fa fa-twitter"></i></a><a href="http://themes.profteamsolutions.com/selfie-multi/" class="linkedin"><i class="fa fa-linkedin"></i></a>
+								</div>
+								<div class="member_about">
+									 In et massa a massa egestas suscipit tincidunt ut est. Curabitur rutrum faucibus elit, at convallis diam.
+								</div>
+								<div class="skill-member">
+									<ul class="skillBar">
+										<li style="opacity: 1; left: 0px;">
+									<div class="skillBg">
+										<span data-width="90" style="width: 90%;"><strong>Photoshop 90%</strong></span>
+									</div>
+								</li><li style="opacity: 1; left: 0px;">
+									<div class="skillBg">
+										<span data-width="85" style="width: 85%;"><strong>HTML/CSS 85%</strong></span>
+									</div>
+								</li><li style="opacity: 1; left: 0px;">
+									<div class="skillBg">
+										<span data-width="92" style="width: 92%;"><strong>Management 92%</strong></span>
+									</div>
+								</li>
+									</ul>
+								</div>
+							</div>
+							</div>
+							</div><div class="col-md-3 col-sm-6 col-xs-12">
+							<div class="member_item">
+							<div class="member_img">
+								<img width="270" height="270" src="http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-4.jpg" class="attachment-identity-team size-identity-team wp-post-image" alt="Team-4" srcset="http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-4-150x150.jpg 150w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-4-75x75.jpg 75w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-4-220x220.jpg 220w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-4-180x180.jpg 180w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-4.jpg 270w" sizes="(max-width: 270px) 100vw, 270px">
+							</div>
+							<div class="member_descr center">
+								<div class="member_name">
+									Tomy Doe
+								</div>
+								<div class="member_post">
+									 Web Developer
+								</div>
+								<div class="member_social">
+								<a href="http://themes.profteamsolutions.com/selfie-multi/" class="pinterest"><i class="fa fa-pinterest"></i></a><a href="http://themes.profteamsolutions.com/selfie-multi/" class="facebook"><i class="fa fa-facebook"></i></a><a href="http://themes.profteamsolutions.com/selfie-multi/" class="gplus"><i class="fa fa-google-plus"></i></a>
+								</div>
+								<div class="member_about">
+									 In et massa a massa egestas suscipit tincidunt ut est. Curabitur rutrum faucibus elit, at convallis diam.
+								</div>
+								<div class="skill-member">
+									<ul class="skillBar">
+										<li style="opacity: 1; left: 0px;">
+									<div class="skillBg">
+										<span data-width="79" style="width: 79%;"><strong>Photoshop 79%</strong></span>
+									</div>
+								</li><li style="opacity: 1; left: 0px;">
+									<div class="skillBg">
+										<span data-width="85" style="width: 85%;"><strong>PHP 85%</strong></span>
+									</div>
+								</li><li style="opacity: 1; left: 0px;">
+									<div class="skillBg">
+										<span data-width="94" style="width: 94%;"><strong>CSS 94%</strong></span>
+									</div>
+								</li>
+									</ul>
+								</div>
+							</div>
+							</div>
+							</div><div class="col-md-3 col-sm-6 col-xs-12">
+							<div class="member_item">
+							<div class="member_img">
+								<img width="270" height="270" src="http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-3.jpg" class="attachment-identity-team size-identity-team wp-post-image" alt="Team-3" srcset="http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-3-150x150.jpg 150w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-3-75x75.jpg 75w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-3-220x220.jpg 220w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-3-180x180.jpg 180w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-3.jpg 270w" sizes="(max-width: 270px) 100vw, 270px">
+							</div>
+							<div class="member_descr center">
+								<div class="member_name">
+									Susan Doe
+								</div>
+								<div class="member_post">
+									 CEO / Founder
+								</div>
+								<div class="member_social">
+								<a href="http://themes.profteamsolutions.com/selfie-multi/" class="facebook"><i class="fa fa-facebook"></i></a><a href="http://themes.profteamsolutions.com/selfie-multi/" class="linkedin"><i class="fa fa-linkedin"></i></a><a href="http://themes.profteamsolutions.com/selfie-multi/" class="gplus"><i class="fa fa-google-plus"></i></a>
+								</div>
+								<div class="member_about">
+									 In et massa a massa egestas suscipit tincidunt ut est. Curabitur rutrum faucibus elit, at convallis diam.
+								</div>
+								<div class="skill-member">
+									<ul class="skillBar">
+										<li style="opacity: 1; left: 0px;">
+									<div class="skillBg">
+										<span data-width="85" style="width: 85%;"><strong>Photoshop 85%</strong></span>
+									</div>
+								</li><li style="opacity: 1; left: 0px;">
+									<div class="skillBg">
+										<span data-width="91" style="width: 91%;"><strong>HTML/CSS 91%</strong></span>
+									</div>
+								</li><li style="opacity: 1; left: 0px;">
+									<div class="skillBg">
+										<span data-width="86" style="width: 86%;"><strong>SEO 86%</strong></span>
+									</div>
+								</li>
+									</ul>
+								</div>
+							</div>
+							</div>
+							</div><div class="col-md-3 col-sm-6 col-xs-12">
+							<div class="member_item">
+							<div class="member_img">
+								<img width="500" height="500" src="http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-1.jpg" class="attachment-identity-team size-identity-team wp-post-image" alt="Team-1" srcset="http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-1-150x150.jpg 150w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-1-300x300.jpg 300w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-1-75x75.jpg 75w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-1-220x220.jpg 220w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-1-380x380.jpg 380w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-1-180x180.jpg 180w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-1-400x400.jpg 400w, http://themes.profteamsolutions.com/selfie-multi/wp-content/uploads/2016/01/Team-1.jpg 500w" sizes="(max-width: 500px) 100vw, 500px">
+							</div>
+							<div class="member_descr center">
+								<div class="member_name">
+									Jane Doe
+								</div>
+								<div class="member_post">
+									 PHP Developer
+								</div>
+								<div class="member_social">
+								<a href="http://themes.profteamsolutions.com/selfie-multi/" class="facebook"><i class="fa fa-facebook"></i></a><a href="http://themes.profteamsolutions.com/selfie-multi/" class="pinterest"><i class="fa fa-pinterest"></i></a><a href="http://themes.profteamsolutions.com/selfie-multi/" class="flickr"><i class="fa fa-flickr"></i></a>
+								</div>
+								<div class="member_about">
+									 In et massa a massa egestas suscipit tincidunt ut est. Curabitur rutrum faucibus elit, at convallis diam.
+								</div>
+								<div class="skill-member">
+									<ul class="skillBar">
+										<li style="opacity: 1; left: 0px;">
+									<div class="skillBg">
+										<span data-width="77" style="width: 77%;"><strong>Photoshop 77%</strong></span>
+									</div>
+								</li><li style="opacity: 1; left: 0px;">
+									<div class="skillBg">
+										<span data-width="78" style="width: 78%;"><strong>Painting 78%</strong></span>
+									</div>
+								</li><li style="opacity: 1; left: 0px;">
+									<div class="skillBg">
+										<span data-width="95" style="width: 95%;"><strong>Management 95%</strong></span>
+									</div>
+								</li>
+									</ul>
+								</div>
+							</div>
+							</div>
+							</div></div></div></div></div></div></div></div></div></div>
+
+<!-- Get Page Footer
+================================================== -->
+<?php get_footer('nuevo'); ?>
